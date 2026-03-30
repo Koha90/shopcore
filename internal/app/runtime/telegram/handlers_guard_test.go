3813f475c1f87a4ea9e7b8087ef4871b418500ad
koha90 <koha90@git.com>
2026-03-30 12:49:51 +0500

    added handlers_guard_test.go

---
+++ internal/app/runtime/telegram/handlers_guard_test.go
@@ -0,0 +1,98 @@
+package telegram
+
+import (
+	"context"
+	"io"
+	"log/slog"
+	"testing"
+
+	"github.com/go-telegram/bot/models"
+	"github.com/stretchr/testify/require"
+
+	"github.com/koha90/shopcore/internal/manager"
+)
+
+func newTestRunner() *Runner {
+	return &Runner{
+		log: slog.New(slog.NewTextHandler(io.Discard, nil)),
+	}
+}
+
+func TestStartHandler_IgnoresNilMessage(t *testing.T) {
+	r := newTestRunner()
+	h := r.startHandler(manager.BotSpec{
+		ID:            "bot-1",
+		Name:          "Shop Bot",
+		StartScenario: "reply_welcome",
+	})
+
+	require.NotPanics(t, func() {
+		h(context.Background(), nil, &models.Update{})
+	})
+}
+
+func TestCallbackHandler_IgnoresNilCallbackQuery(t *testing.T) {
+	r := newTestRunner()
+	h := r.callbackHandler(manager.BotSpec{
+		ID:            "bot-2",
+		Name:          "Shop Bot",
+		StartScenario: "inline_catalog",
+	})
+
+	require.NotPanics(t, func() {
+		h(context.Background(), nil, &models.Update{})
+	})
+}
+
+func TestDefaultHandler_IgnoresNilMessage(t *testing.T) {
+	r := newTestRunner()
+	h := r.defaultHandler(manager.BotSpec{
+		ID:            "bot-3",
+		Name:          "Shop Bot",
+		StartScenario: "reply_welcome",
+	})
+
+	require.NotPanics(t, func() {
+		h(context.Background(), nil, &models.Update{})
+	})
+}
+
+func TestDefaultHandler_IgnoresUnknownReplyText(t *testing.T) {
+	r := newTestRunner()
+	h := r.defaultHandler(manager.BotSpec{
+		ID:            "bot-4",
+		Name:          "Shop Bot",
+		StartScenario: "reply_welcome",
+	})
+
+	update := &models.Update{
+		Message: &models.Message{
+			Text: "какая-то непонятная команда",
+			Chat: models.Chat{
+				ID: 101,
+			},
+		},
+	}
+
+	require.NotPanics(t, func() {
+		h(context.Background(), nil, update)
+	})
+}
+
+func TestErrorsHandler_DoesNotPanic(t *testing.T) {
+	r := newTestRunner()
+	h := r.errorsHandler(manager.BotSpec{
+		ID:   "bot-5",
+		Name: "Shop Bot",
+	})
+
+	require.NotPanics(t, func() {
+		h(assertErr{})
+	})
+}
+
+type assertErr struct{}
+
+func (assertErr) Error() string {
+	return "boom"
+}
