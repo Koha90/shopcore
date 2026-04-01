e19ef43789a4a2970d91595223730b734e4b6310
koha90 <koha90@git.com>
2026-04-01 23:07:05 +0500

    deleted old migration files

--- migrations/0002_districts_create_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS districts (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  city_id INT NOT NULL REFERENCES cities(id) ON DELETE CASCADE,
-  name TEXT NOT NULL,
-  UNIQUE(city_id, name)
-);
-
-CREATE INDEX IF NOT EXISTS idx_districts_city_id ON districts(city_id);
